e245509843da04af7e154c06a8458ddff0f57703
Anup Kumar Gupta <anup@troopr.io>
2019-06-22 16:37:03 +0530

    navbar-design

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -5,8 +5,16 @@ import './common.css';
 class NavBar extends Component {
     render() {
         return (
-            <div>
-                Nav Bar
+            <div className="d-flex justify-content-between align-items-center navbar-container text-bold">
+                <div className="d-flex active-item">
+                    <div className="text-2">2</div>
+                    <div className="text-rem">gethr</div>
+                </div>
+                <div className="d-flex right-navbar-container">
+                    <div className="right-navbar-item">Home</div>
+                    <div className="right-navbar-item">About</div>
+                    <div className="right-navbar-item">Contact Us</div>
+                </div>
             </div>
         )
     }
